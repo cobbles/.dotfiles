b47e13ab221c5232b1498a1c484ac3800c641cb5
Tom Molloy <cobbles@molloy.cloud>
2024-12-17 21:36:59 +1100

    add file navigation and git plugins

---
+++ files/.config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,4 @@
+-- Git commands.
+
+return { 'tpope/vim-fugitive', dependencies = 'tpope/vim-rhubarb' }
+
---
+++ files/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+-- Git integration
+
+return {
+  'lewis6991/gitsigns.nvim',
+  lazy = false,
+  opts = {
+    signcolumn = true,
+    current_line_blame = true,
+  }
+}
