84dce934a48fb30b10e75e785e719b6f208d3eda
Tom Molloy <cobbles@molloy.cloud>
2024-12-19 20:50:29 +1100

    improve syntax highlighting

--- files/.config/nvim/lua/plugins/treesitter.lua
+++ files/.config/nvim/lua/plugins/treesitter.lua
@@ -9,14 +9,33 @@ return {
         'JoosepAlviste/nvim-ts-context-commentstring',
       }
     },
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = 'all',
-        auto_intsall = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        additional_vim_regex_highlighting = true
+    opts = {
+      ensure_installed = 'all',
+      auto_intsall = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      additional_vim_regex_highlighting = false,
+      context_commentstring = { enabled = true }
+    },
+    config = function(_, opts)
+      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = {"src/parser.c"},
+          branch = "main",
+        },
+        filetype = "blade"
       }
+
+      vim.filetype.add({
+        pattern = {
+          ['.*%.blade%.php'] = 'blade',
+        },
+      })
+
+      require'nvim-treesitter.configs'.setup(opts)
     end
   },
 }
