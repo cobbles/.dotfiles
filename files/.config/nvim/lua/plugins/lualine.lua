db475987e46c5b9bfbee44f0edec0315c8c11f20
Tom Molloy <cobbles@molloy.cloud>
2024-12-20 17:55:10 +1100

    begin setting up lsps

--- files/.config/nvim/lua/plugins/lualine.lua
+++ files/.config/nvim/lua/plugins/lualine.lua
@@ -13,5 +13,18 @@ return {
       component_separators = '|',
       section_separators = ''
     }
+  },
+  sections = {
+    lualine_b = {
+        'branch',
+        {
+          'diff',
+          symbols = { added = ' ', modified = ' ', removed = ' ' },
+        },
+        function ()
+          return '󰅭 ' .. vim.pesc(tostring(#vim.tbl_keys(vim.lsp.buf_get_clients())) or '')
+        end,
+        { 'diagnostics', sources = { 'nvim_diagnostic' } },
+      },
   }
 }
