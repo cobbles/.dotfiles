3604d1e6611d64334624fd1841c60e726c2742c6
Tom Molloy <cobbles@molloy.cloud>
2025-04-17 08:30:26 +1000

    fix telescope greg commands

--- files/.config/nvim/lua/plugins/telescope.lua
+++ files/.config/nvim/lua/plugins/telescope.lua
@@ -6,10 +6,10 @@ return {
     },
     keys = {
       { '<leader>f', function() require('telescope.builtin').find_files({hidden = true, prompt_title = 'Files' }) end },
-      { '<leader>F', function() require('telescope.builtin').find_files({ hidden = true, no_ignore = true, prompt_title = 'All Files' }) end },
+      { '<leader>F', function() require('telescope.builtin').find_files({ hidden = true, no_ignore = true, no_ignore_parent = true, prompt_title = 'All Files' }) end },
       { '<leader>b', function() require('telescope.builtin').buffers() end },
-      { '<leader>g', function() require('telescope.builtin').live_grep({hidden = true, prompt_title = 'Grep Files' }) end },
-      { '<leader>G', function() require('telescope.builtin').live_grep({hidden = true, no_ignore = true, prompt_title = 'Grep All Files' }) end },
+      { '<leader>g', function() require('telescope.builtin').live_grep({additional_args = function() return { "--hidden" } end, prompt_title = 'Grep Files' }) end },
+      { '<leader>G', function() require('telescope.builtin').live_grep({additional_args = function() return { "--hidden", "--no-ignore" } end, prompt_title = 'Grep All Files' }) end },
       { '<leader>h', function() require('telescope.builtin').help_tags() end },
       { '<leader>s', function() require('telescope.builtin').lsp_document_symbols() end },
     }
