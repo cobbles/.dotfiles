3aa0bf0ebb8b4c9951d7c18b8bb097a59f2c2d60
Tom Molloy <cobbles@molloy.cloud>
2025-12-03 23:00:40 +1100

    clean up plugins at greeter and cheatsheet

---
+++ files/.config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,32 @@
+return {
+    'goolord/alpha-nvim',
+    dependencies = {
+        'nvim-mini/mini.icons',
+        'nvim-lua/plenary.nvim'
+    },
+    config = function ()
+        local dashboard = require'alpha.themes.dashboard'
+        -- Set header
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+        }
+        -- Set menu
+        dashboard.section.buttons.val = {
+            dashboard.button( "space g", "  > Search" , ""),
+            dashboard.button( "space f", "  > Find File", ""),
+            dashboard.button( "o", "  > File Tree", ":Oil<CR>"),
+            dashboard.button( "p", "  > Plugins", ":Lazy<CR>"), 
+            dashboard.button( "l", "  > LSPs", ":Mason<CR>"),
+            dashboard.button( "space ?", "󰋗  > Help" , ""),
+            dashboard.button( "q", "󰩈  > Quit", ":q<CR>"),
+        }
+        require'alpha'.setup(dashboard.config)
+    end
+};
---
+++ files/.config/nvim/lua/plugins/cheatsheet.lua
@@ -0,0 +1,12 @@
+return {
+  'sudormrfbin/cheatsheet.nvim',
+  opts = {
+    include_only_installed_plugins = true,
+  },
+
+  requires = {
+    {'nvim-telescope/telescope.nvim'},
+    {'nvim-lua/popup.nvim'},
+    {'nvim-lua/plenary.nvim'},
+  }
+}
